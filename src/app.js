75acba3331bc147f8920f9df379c868a49375dcc
tanisqcodes <the.crektek@gmail.com>
2025-10-23 23:24:55 +0530

    added configuration for app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
 import express from "express"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 // Enable CORS for all routes
-app.use(cors({
+app.use(cors({ // currently app accepts requests from vite-react server
     origin: 'http://localhost:5173', // Vite default port
     credentials: true
 }))
 
-// Parse JSON bodies
-app.use(express.json())
+// parse JSON bodies
+app.use(express.json()) 
 
+// serve the static HTML site from the public_html directory
+app.use(express.static(path.join(__dirname, '..', '..', 'public_html'))) 
+
+
+// serve the React app from the frontend/dist directory
+app.use("/app", express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')))
+
+// backend api routes
 import {router} from "./routes/mainrouter.router.js"
-app.use("/user", router)
+app.use("/api/user", router)
+
+// If a user deep-links (e.g., refreshes /app/dashboard),
+// this makes sure to send them the main index.html so React Router can take over.
+app.get('/app/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
+});
 
 
 export {app}
