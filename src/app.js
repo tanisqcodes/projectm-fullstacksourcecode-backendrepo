9a32a8b39b519218c1d94d4ee3ad658caf3c78eb
tanisqcodes <the.crektek@gmail.com>
2025-10-27 01:12:12 +0530

    ready to be put on aws beanstalk

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import express from "express"
 import cors from "cors"
+import helmet from "helmet"
 
 const app = express()
+app.use(helmet())
 
 // Enable CORS for all routes
-app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
-    credentials: true
-}))
+app.use(cors())
 
 // Parse JSON bodies
 app.use(express.json())
 
 import {router} from "./routes/mainrouter.router.js"
 app.use("/user", router)
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
+});
 
 
 export {app}
